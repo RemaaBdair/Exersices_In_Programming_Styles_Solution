55b33a69749fd6c6dc4e70dbdd72131b47e18746
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-03-09 17:39:25 +0200

    q2 of chapter11

---
+++ Chapter11/index2.ts
@@ -0,0 +1,130 @@
+const fs = require("fs");
+class LetterBox {
+  static info(name) {
+    console.log(name);
+  }
+}
+class DataStorageManager {
+  data: string;
+  dispatch(message: string, path?: string) {
+    switch (message) {
+      case "init":
+        this.init(path);
+        break;
+      case "words":
+        return this.words();
+      case "info":
+        LetterBox.info(this.constructor.name);
+        break;
+      default:
+        throw new Error("Can't understand the mesage");
+    }
+  }
+  init(path: string) {
+    this.data = fs
+      .readFileSync(path, "utf8")
+      .replace(/[^a-zA-Z]/g, " ")
+      .toLocaleLowerCase();
+  }
+  words(): string[] {
+    return this.data.split(" ");
+  }
+}
+class StopWordsManager {
+  stopWords: string[];
+  dispatch(message: string, word?: string) {
+    switch (message) {
+      case "init":
+        this.init();
+        break;
+      case "stopWords":
+        return this.is_Not_stop_word(word);
+      case "info":
+        LetterBox.info(this.constructor.name);
+        break;
+      default:
+        throw new Error("Can't understand the mesage");
+    }
+  }
+  init() {
+    this.stopWords = fs.readFileSync("stop_words.txt", "utf8").split(",");
+  }
+  is_Not_stop_word(word: string): boolean {
+    return this.stopWords.indexOf(word) == -1 && word.length != 0;
+  }
+}
+class WordFrequencyManager {
+  freqCount: Record<string, number>;
+  dispatch(message: string, word?: string) {
+    switch (message) {
+      case "init":
+        this.init();
+        break;
+      case "increment":
+        this.increment_count(word);
+        break;
+      case "sort":
+        return this.sort();
+      case "info":
+        LetterBox.info(this.constructor.name);
+        break;
+      default:
+        throw new Error("Can't understand the mesage");
+    }
+  }
+  init() {
+    this.freqCount = {};
+  }
+  increment_count(word: string) {
+    if (this.freqCount[word]) {
+      this.freqCount[word]++;
+    } else {
+      this.freqCount[word] = 1;
+    }
+  }
+  sort() {
+    return Object.entries(this.freqCount).sort((a, b) => b[1] - a[1]);
+  }
+}
+class WordFrequencyController {
+  wordFrequencyManager: WordFrequencyManager;
+  stopWordsManager: StopWordsManager;
+  dataStorageManager: DataStorageManager;
+  dispatch(message: string, path?: string) {
+    switch (message) {
+      case "init":
+        this.init(path);
+        break;
+      case "run":
+        this.run();
+        break;
+      case "info":
+        LetterBox.info(this.constructor.name);
+        break;
+      default:
+        throw new Error("Can't understand the mesage");
+    }
+  }
+  init(path: string) {
+    this.dataStorageManager = new DataStorageManager();
+    this.dataStorageManager.dispatch("info");
+    this.stopWordsManager = new StopWordsManager();
+    this.stopWordsManager.dispatch("info");
+    this.wordFrequencyManager = new WordFrequencyManager();
+    this.wordFrequencyManager.dispatch("info");
+    this.dataStorageManager.dispatch("init", path);
+    this.stopWordsManager.dispatch("init");
+    this.wordFrequencyManager.dispatch("init");
+  }
+  run() {
+    this.dataStorageManager.words().forEach(word => {
+      if (this.stopWordsManager.dispatch("stopWords", word))
+        this.wordFrequencyManager.dispatch("increment", word);
+    });
+    console.log(this.wordFrequencyManager.sort());
+  }
+}
+let wordFrequencyController = new WordFrequencyController();
+wordFrequencyController.dispatch('info');
+wordFrequencyController.dispatch("init", "input_words.txt");
+wordFrequencyController.dispatch("run");
