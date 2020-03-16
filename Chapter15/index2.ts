b62c8bb8c8ff21edf6eae69a31c23c520125ee01
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-03-16 20:45:57 +0200

    q2 of chapter15

---
+++ Chapter15/index2.ts
@@ -0,0 +1,119 @@
+import { eventNames } from "cluster";
+
+const fs = require("fs");
+class EventManager {
+  subscriptionsList: {};
+  constructor() {
+    this.subscriptionsList = {};
+  }
+  subscribe(eventName: string, handler) {
+    if (!this.subscriptionsList[eventName])
+      this.subscriptionsList[eventName] = [handler];
+    else this.subscriptionsList[eventName].push(handler);
+  }
+  publish(eventName: string, arg) {
+    if (this.subscriptionsList[eventName]) {
+      for (let handler of this.subscriptionsList[eventName]) handler(arg);
+    }
+  }
+}
+
+class DataStorage {
+  data: string;
+  eventManager: EventManager;
+  constructor(eventManager: EventManager) {
+    this.eventManager = eventManager;
+    eventManager.subscribe("loadFile", this.load);
+    eventManager.subscribe("beginWork", this.wordsGenerator);
+  }
+  load = (path: string) => {
+    this.data = fs
+      .readFileSync(path, "utf8")
+      .replace(/[^a-zA-Z]/g, " ")
+      .toLocaleLowerCase();
+  };
+  wordsGenerator = () => {
+    this.data
+      .split(" ")
+      .forEach(w => this.eventManager.publish("generatedWords", w));
+    this.eventManager.publish("end", null);
+  };
+}
+class StopWordFilter {
+  stopWords: string[];
+  eventManager: EventManager;
+  constructor(eventManager: EventManager) {
+    this.eventManager = eventManager;
+    this.stopWords = [];
+    this.eventManager.subscribe("loadFile", this.load);
+    this.eventManager.subscribe("generatedWords", this.isNotAStopWord);
+  }
+  load = () => {
+    this.stopWords = fs.readFileSync("stop_words.txt", "utf8").split(",");
+  };
+  isNotAStopWord = (word: string) => {
+    if (this.stopWords.indexOf(word) == -1 && word.length != 0)
+      this.eventManager.publish("notAstopWord", word);
+  };
+}
+class WordFrequencyCounter {
+  freqCount: Record<string, number>;
+  eventManager: EventManager;
+  constructor(eventManager: EventManager) {
+    this.freqCount = {};
+    this.eventManager = eventManager;
+    this.eventManager.subscribe("notAstopWord", this.incrementFrequency);
+    this.eventManager.subscribe("print", this.print);
+  }
+  incrementFrequency = (word: string) => {
+    if (this.freqCount[word]) {
+      this.freqCount[word]++;
+    } else {
+      this.freqCount[word] = 1;
+    }
+  };
+  print = () => {
+    let sortedWords = Object.entries(this.freqCount).sort(
+      (a, b) => b[1] - a[1]
+    );
+    for (let w of sortedWords) console.log(w);
+  };
+}
+class NonStopWordsWithZ {
+  counter: number = 0;
+  eventManager: EventManager;
+  constructor(eventManager: EventManager) {
+    this.eventManager=eventManager;
+    this.eventManager.subscribe("notAstopWord", this.count);
+    this.eventManager.subscribe("print", this.print);
+  }
+  count = (word: string) => {
+    if (word.includes("z")) this.counter++;
+  };
+  print = () => {
+    console.log("The count of non-stop words with z=", this.counter);
+  };
+}
+class WordFrequencyApp {
+  eventManager: EventManager;
+  constructor(eventManager: EventManager) {
+    this.eventManager = eventManager;
+    this.eventManager.subscribe("start", this.start);
+    this.eventManager.subscribe("end", this.end);
+  }
+  start = (path:string) => {
+    this.eventManager.publish("loadFile", path);
+    this.eventManager.publish("beginWork", null);
+  };
+  end = () => {
+    this.eventManager.publish("print", null);
+  };
+}
+
+let eventManager = new EventManager();
+let dataStorage = new DataStorage(eventManager);
+let stopWordFilter = new StopWordFilter(eventManager);
+let wordFrequencyCounter = new WordFrequencyCounter(eventManager);
+let nonStopWordsWithZ= new NonStopWordsWithZ(eventManager)
+let wordFrequencyApp = new WordFrequencyApp(eventManager);
+eventManager.publish("start", "input_Words.txt");
