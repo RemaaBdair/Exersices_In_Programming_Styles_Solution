d62155c2dbade7aa614c4faed8885010a0e4617a
RemaaBdair <RemaaBdair@users.noreply.github.com>
2020-03-23 01:09:48 +0200

    replace assert with if stm

--- Chapter22/index3.ts
+++ Chapter22/index3.ts
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const assert= require('assert')
+const assert = require("assert");
 class TheOne {
   value = null;
   constructor(v) {
@@ -10,7 +10,7 @@ class TheOne {
       this.value = func(this.value);
     } catch (e) {
       console.error(e);
-      throw(e)
+      throw e;
     }
   }
   printme() {
@@ -18,39 +18,41 @@ class TheOne {
   }
 }
 function extractWords(filePath: string): string[] {
-    assert(typeof filePath === "string", "Path of the file should be string");
-    assert(filePath, "The path should be a non empty string");
-    let data: string;
-    data = fs.readFileSync(filePath, "utf8");
-    return data
-      .replace(/[^a-zA-Z]/g, " ")
-      .toLocaleLowerCase()
-      .split(" ");
-  }
-  function removeStopWords(data: string[]): string[] {
-    let stopWords: string[];
-    assert(data.constructor === Array, "Data should be an array");
-    stopWords = fs.readFileSync("stop_words.txt", "utf8").split(",");
-    return data.filter(word => {
-      return stopWords.indexOf(word) == -1 && word.length != 0;
-    });
-  }
-  function calculateFreq(data: string[]): Record<string, number> {
-    let freqCount: Record<string, number> = {};
-    assert(data.constructor === Array, "Data should be an array");
-    data.forEach(word => {
-      if (freqCount[word]) {
-        freqCount[word]++;
-      } else {
-        freqCount[word] = 1;
-      }
-    });
-    return freqCount;
-  }
-  function sortArray(data: Record<string, number>): [string, number][] {
-    assert(typeof data === "object", "Data should be an {}");
-    return Object.entries(data).sort((a, b) => b[1] - a[1]);
-  }
+  if (typeof filePath !== "string")
+    throw new Error("Path of the file should be string");
+  if (!filePath) throw new Error("The path should be a non empty string");
+  let data: string;
+  data = fs.readFileSync(filePath, "utf8");
+  return data
+    .replace(/[^a-zA-Z]/g, " ")
+    .toLocaleLowerCase()
+    .split(" ");
+}
+function removeStopWords(data: string[]): string[] {
+  let stopWords: string[];
+  if (Array.isArray(data)) throw new Error("Data should be an array");
+  stopWords = fs.readFileSync("stop_words.txt", "utf8").split(",");
+  return data.filter(word => {
+    return stopWords.indexOf(word) == -1 && word.length != 0;
+  });
+}
+function calculateFreq(data: string[]): Record<string, number> {
+  let freqCount: Record<string, number> = {};
+  if (Array.isArray(data)) throw new Error("Data should be an array");
+  data.forEach(word => {
+    if (freqCount[word]) {
+      freqCount[word]++;
+    } else {
+      freqCount[word] = 1;
+    }
+  });
+  return freqCount;
+}
+function sortArray(data: Record<string, number>): [string, number][] {
+  if (data === null || typeof data !== "object" || Array.isArray(data))
+    throw new Error("Data should be an {}");
+  return Object.entries(data).sort((a, b) => b[1] - a[1]);
+}
 let x = new TheOne("input_words.txt");
 x.bind(extractWords);
 x.bind(removeStopWords);
